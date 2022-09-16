b00e1858b75641ef766aaa62d033fbf5e6fdea2c
wyaaung <wya.aung@gmail.com>
2022-09-16 23:54:40 +0100

    Moved into one file

--- Creational_Patterns/Singleton/singleton.cpp
+++ Creational_Patterns/Singleton/singleton.cpp
@@ -1,11 +1,44 @@
-#include "singleton.h"
+#include <iostream>
 
 using namespace std;
 
+class Singleton{
+    protected:
+        static Singleton* singleton_;
+        /**
+         * Constructor in protected.
+         */
+        Singleton();
+
+    public:
+        /**
+         * Singletons should not be cloneable.
+         */
+        Singleton(Singleton &other) = delete;
+        /**
+         * Singletons should not be assignable.
+         */
+        void operator=(const Singleton &) = delete;
+
+
+        static Singleton *get_instance();
+};
+
 Singleton *Singleton::get_instance(){
-    if (singleton == nullptr){
-        singleton = new Singleton();
+    if (singleton_ == nullptr){
+        singleton_ = new Singleton();
     }
     
-    return singleton;
+    return singleton_;
+}
+
+int main(){
+    Singleton* s1 = s1->get_instance();
+    Singleton* s2 = s2->get_instance();
+
+    bool check = s1 == s2;
+
+    cout << (check ? "True" : "False") << "\n";
+    
+    return 0;
 }
