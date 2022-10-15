7bb6c5878d01c4dc7be5779c1b5c6c0693c9e394
wyaaung <wya.aung@gmail.com>
2022-10-15 16:17:33 +0100

    Binary Insertion Sort (Iterative in BinarySearch)

--- sorting/binary_insertion_sort.cpp
+++ sorting/binary_insertion_sort.cpp
@@ -22,22 +22,20 @@ void swap(int *x, int *y){
 // A binary search based function to find the position
 // where item should be inserted in arr[low..high]
 int binarySearch(vector<int> &arr, int search, int start, int end){
-    if (end <= start){
-        return (search > arr[start]) ? (start + 1) : start;
+    while (start <= end){
+        int mid = start + (end - start) / 2;
+
+        if (search == arr[mid]){
+            return mid + 1;
+        }else if (search > arr[mid]){
+            start = mid + 1;
+        }else {
+            end = mid - 1;
+        }
     }
-
-    int mid = start + (end - start) / 2;
-
-    if (search == arr[mid]){
-        return mid + 1;
-    }
-
-    if (search < arr[mid]){
-        return binarySearch(arr, search, start, mid - 1);
-    }
-
-    return binarySearch(arr, search, mid + 1, end);
+    return start;
 }
+
 void binaryInsertionSort(vector<int> &arr){
     int key, j, location;
 
