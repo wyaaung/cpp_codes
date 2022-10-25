a131d41a556b48284568571c6478710f2eeb2dc0
wyaaung <wya.aung@gmail.com>
2022-10-25 22:32:20 +0100

    Initiated to implement doubly linked list

--- data_structures/linked_list/doubly_linked_list.h
+++ data_structures/linked_list/doubly_linked_list.h
@@ -8,6 +8,9 @@ class Node {
         T value;
         Node<T> *next, *prev;
 
+        // A friend class can access private and protected members of other class 
+        // in which it is declared as friend.
+        friend class DoublyLinkedList;
     public:
         Node(T value);
         Node(T value, Node<T> *&next, Node<T> *&prev);
@@ -18,3 +21,39 @@ template<class T> void swap(T& x, T& y){
     x = y;
     y = temp;
 }
+
+// Doubly Linked List 
+template <typename T>
+class DoublyLinkedList {
+    private:
+        int size = 0;
+
+        Node<T> *head = nullptr;
+        Node<T> *tail = nullptr;
+
+    public:
+        DoublyLinkedList();         // Constructor
+
+        ~DoublyLinkedList();        // Destructor
+
+        bool empty() const;         // is list empty?
+
+        const T& head() const;      // get head node
+
+        const T& tail() const;       // get tail node
+
+        // Get read access to node, but cannot modify its value (becuz of const).
+        // Thread Safe
+        void addFront(const T& value);   // add to the head of list
+
+        void addBack(const T& value);    // add to tail of the list
+
+        void removeFront();             // remove from head
+
+        void removeBack();              // remove from tail
+
+    protected:
+        void add(Node<T> *node, const T& value);       // insert new node after before "node"
+
+        void remove(Node<T> *node);         // remove node "node"
+};
