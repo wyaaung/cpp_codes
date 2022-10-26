be42190323ad4cced404fb5adffcf1cc494175ba
wyaaung <wya.aung@gmail.com>
2022-10-26 01:46:57 +0100

    Impl of Document

---
+++ library_system/document.cpp
@@ -0,0 +1,47 @@
+#include "document.h"
+
+/*Setting the title of the document*/
+void Document::updateTitle(const char *newTitle) {
+    free(_title);
+    _title = (char*) malloc((strlen(newTitle)+1) * sizeof(char));
+    strcpy(_title, newTitle);
+}
+
+/*Setting the year of the document*/
+void Document::updateYear(int newYear){
+    _year = newYear;
+}
+
+/*Setting the quantity of the document*/
+void Document::updateQuantity(int newQuantity){
+    _quantity = newQuantity;
+}
+
+/*Getter for the title of the document*/
+char* Document::getTitle(){
+    return _title;
+}
+
+/*Getter for the year of the document*/
+int Document::getYear(){
+    return _year;
+}
+
+/*Getter for the quantity of the document*/
+int Document::getQuantity(){
+    return _quantity;
+}
+
+/*Function for borrowing the document. reduces the quantity of the document by one.*/
+int Document::borrowDoc(){
+    if (_quantity > 0){
+        updateQuantity(getQuantity() - 1);
+        return 0;
+    }
+    return -1;
+}
+
+/*Function for borrowing the document. increases the quantity of the document by one.*/
+void Document::returnDoc(){
+    updateQuantity(getQuantity() + 1);
+}
